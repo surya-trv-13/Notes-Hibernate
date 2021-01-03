0f542485c5d8b6ecf01e7c660c04d40170e11b02
suryanarayan.rath <suryanarayan.rath@highradius.com>
2021-01-03 20:54:48 +0530

    Created Entity for the notes

---
+++ src/main/java/com/entities/Notes.java
@@ -0,0 +1,46 @@
+package com.entities;
+
+import java.util.Date;
+import java.util.Random;
+
+public class Notes {
+	private int id;
+	private String title;
+	private String content;
+	private Date addedDate;
+	
+	public Notes() {
+		super();
+	}
+	public Notes(String title, String content, Date addedDate) {
+		super();
+		this.id = new Random().nextInt();
+		this.title = title;
+		this.content = content;
+		this.addedDate = addedDate;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getAddedDate() {
+		return addedDate;
+	}
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+}
